2c90d3adbdb59ec392d3b58ffbb3b021c9ad26bd
Jungea <jungea2241@gmail.com>
2020-06-27 01:48:39 +0900

    Multiple Page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a href="page1.html">페이지 1</a> <br />
+    <a href="page2.html">페이지 2</a> <br />
+    <a href="page3.html">페이지 3</a> <br />
+    <a href="page4.html">페이지 4</a> <br />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/pages/page1/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>페이지1</h1>
+    <Counter1 title="카운터1" description="첫째 컴포넌트" />
+    <Counter1 title="카운터1" description="첫째 컴포넌트" />
+  </div>
+</template>
+
+<script>
+import Counter1 from './Counter1.vue'
+
+export default {
+  name: 'App',
+  components : { Counter1 }
+}
+</script>
