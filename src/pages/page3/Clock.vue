cb45134e3cdae3edd292f2f90331b33ec27fae10
Jungea <jungea2241@gmail.com>
2020-06-27 17:11:18 +0900

    컴포넌트, font awesome 라이브러리

--- src/pages/page3/Clock.vue
+++ src/pages/page3/Clock.vue
@@ -7,20 +7,28 @@ import moment from "moment";
 
 export default {
   name: "Clock",
+  props: {
+    format: { type: String, default: "HH:mm:ss" }
+  },
   data: function() {
     return { time: null };
   },
   mounted: function() {
-    let callback = () => this.time = moment().format("HH:mm:ss")
+    let callback = () => {
+      this.time = moment().format(this.format)
+      // console.log(this.time)
+    }
     callback();
     this.timerId = setInterval(callback, 1000);
+    // console.log('mounted');
   },
   beforeDestroy: function() {
     clearInterval(this.timerId);
+    // console.log('beforeDestory');
   }
 }
 </script>
 
 <style scoped>
-div { display: inline-block; font-size: 15pt; padding:3px 12px; border: 1px solid gray; }
+div { display: inline-block; }
 </style>
